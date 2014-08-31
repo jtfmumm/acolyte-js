9ade4fd8d7223242826057d44497175e4f6b9c0c
John Mumm <jtfmumm@gmail.com>
2014-08-31 18:06:17 -0400

    Add color to htmlDisplay

--- src/app/js/data/HTMLCodeTable.js
+++ src/app/js/data/HTMLCodeTable.js
@@ -1,11 +1,65 @@
 define(function(require) {
     return {
-        "self": " @",
-        "wall": " #",
-        "palm": " (",
-        "plains": " .",
-        "forest": " F",
-        "drunk": " &",
-        "void": "&nbsp;"
+        "self": {
+            symbol: " @",
+            color: "highlight-back"
+        },
+        "wall": {
+            symbol: " #"
+        },
+        "palm": {
+            symbol: " ("
+        },
+        "plains": {
+            symbol: " ."
+        },
+        "forest": {
+            symbol: " F",
+            color: "dark-light-green-back"
+        },
+        "drunk": {
+            symbol: " &"
+        },
+        "void": {
+            symbol: "&nbsp;&nbsp;",
+            color: "black-back"
+        },
+        //Heights
+        0: {
+            symbol: "&nbsp;&nbsp;",
+            color: "light-tan-back"
+        },
+        1: {
+            symbol: "&nbsp;&nbsp;",
+            color: "mid-light-tan-back"
+        },
+        2: {
+            symbol: "&nbsp;&nbsp;",
+            color: "dark-light-tan-back"
+        },
+        3: {
+            symbol: "&nbsp;&nbsp;",
+            color: "light-green-back"
+        },
+        4: {
+            symbol: "&nbsp;&nbsp;",
+            color: "mid-light-green-back"
+        },
+        5: {
+            symbol: "&nbsp;&nbsp;",
+            color: "dark-light-green-back"
+        },
+        6: {
+            symbol: "&nbsp;&nbsp;",
+            color: "light-brown-back"
+        },
+        7: {
+            symbol: "&nbsp;&nbsp;",
+            color: "mid-light-brown-back"
+        },
+        8: {
+            symbol: "&nbsp;&nbsp;",
+            color: "dark-light-brown-back"
+        }
     }
 });
--- src/app/js/displays/HTMLDisplay.js
+++ src/app/js/displays/HTMLDisplay.js
@@ -5,18 +5,36 @@ define(function(require) {
     var HTMLCodeTable = require("js/data/HTMLCodeTable");
 
     function HTMLDisplay() {
+        this.codeTable = compileCodeTable(HTMLCodeTable);
     }
 
     HTMLDisplay.prototype = {
         renderMap: function(mapCodes) {
+            var that = this;
             var newMap = mapCodes.map(function(code) {
-                return HTMLCodeTable[code];
+                return that.codeTable[code];
             });
 
             var flatMap = newMap.flatJoin("", "<br>");
             $("#display").html(flatMap);
         }
     };
+
+    function compileCodeTable(codeTable) {
+        var terrainKey, newCodeTable = {};
+        for (terrainKey in codeTable) {
+            var entry;
+            var terrain = codeTable[terrainKey];
+            if (terrain.color) {
+                entry = "<span class='" + terrain.color + "'>" + terrain.symbol + "</span>";
+            } else {
+                entry = terrain.symbol;
+            }
+            newCodeTable[terrainKey] = entry;
+        }
+        console.log(newCodeTable);
+        return newCodeTable;
+    }
 
     return HTMLDisplay;
 });
