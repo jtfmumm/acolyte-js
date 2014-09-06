32931f87a22e9f01aa06d78afffb6ffbc76d5feb
John Mumm <jtfmumm@gmail.com>
2014-09-06 16:37:51 -0400

    Optimize html display element creation and rendering

--- src/app/js/displays/HTMLDisplay.js
+++ src/app/js/displays/HTMLDisplay.js
@@ -5,46 +5,51 @@ define(function(require) {
     var HTMLCodeTable = require("js/data/HTMLCodeTable");
 
     function HTMLDisplay() {
-        this.codeTable = compileCodeTable(HTMLCodeTable);
+        this.cache = {};
+        this.displayEl = document.getElementById("display");
     }
 
     HTMLDisplay.prototype = {
         renderMap: function(tileCodes) {
-            var that = this;
+            var _this = this;
+            var newDiv = document.createElement('div');
             var newMap = tileCodes.map(function(tileCode) {
-                return compileHTML(tileCode);
-//                return that.codeTable[code];
+                return _this.compileHTML(tileCode);
             });
 
-            var flatMap = newMap.flatJoin("", "<br>");
-            $("#display").html(flatMap);
+            for (var y = 0; y < newMap.getHeight(); y++) {
+                for (var x = 0; x < newMap.getWidth(); x++) {
+                    newDiv.appendChild(newMap.getCell(x, y));
+                }
+                var br = document.createElement('br');
+                newDiv.appendChild(br);
+            }
+
+            if (this.displayEl.firstChild) {
+                this.displayEl.removeChild(this.displayEl.firstChild);
+            }
+            this.displayEl.appendChild(newDiv);
+        },
+        compileHTML: function(tileCode) {
+            var tileObject = HTMLCodeTable[tileCode.object];
+            var elevation = HTMLCodeTable[tileCode.elevation];
+            var color = tileObject.color ? tileObject.color : "black";
+            var backColor = elevation.color;
+            var symbol = tileObject.symbol;
+
+            var newEl = "" + color + backColor + symbol;
+            if (this.cache[newEl]) {
+                return this.cache[newEl].cloneNode(true);
+            } else {
+                var el = document.createElement('span');
+                el.innerHTML = symbol;
+                el.classList.add(color);
+                el.classList.add("background-" + backColor);
+                this.cache[newEl] = el;
+                return el.cloneNode(true);
+            }
         }
     };
-
-    function compileHTML(tileCode) {
-        var tileObject = HTMLCodeTable[tileCode.object];
-        var elevation = HTMLCodeTable[tileCode.elevation];
-        var color = tileObject.color ? tileObject.color : "black";
-        var backColor = elevation.color;
-        var symbol = tileObject.symbol;
-
-        return "<span class='" + color + " background-" + backColor + "'>" + symbol + "</span>";
-    }
-
-    function compileCodeTable(codeTable) {
-        var terrainKey, newCodeTable = {};
-        for (terrainKey in codeTable) {
-            var entry;
-            var terrain = codeTable[terrainKey];
-            if (terrain.color) {
-                entry = "<span class='" + terrain.color + "'>" + terrain.symbol + "</span>";
-            } else {
-                entry = terrain.symbol;
-            }
-            newCodeTable[terrainKey] = entry;
-        }
-        return newCodeTable;
-    }
 
     return HTMLDisplay;
 });
