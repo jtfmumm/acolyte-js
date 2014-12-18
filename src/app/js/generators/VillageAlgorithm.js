0e67eb66428a9b23ec1051b683bc2b44cfa2b55d
John Mumm <jtfmumm@gmail.com>
2014-12-17 19:53:03 -0500

    Clarify variables

--- src/app/js/generators/VillageAlgorithm.js
+++ src/app/js/generators/VillageAlgorithm.js
@@ -4,30 +4,33 @@ define(function(require) {
     var Coords = require("js/utils/Coords");
     var Matrix = require("js/utils/Matrix");
 
-    var VillageAlgorithm = {
-        generate: function(options) {
-            var diameter = options.diameter;
-            var totalBuildings = Math.floor(diameter / 10);
-            var buildingClearance = 5;
-            var diameterPerRegion = options.diameterPerRegion || diameter;
-            var matrix = new Matrix().init(diameter, diameter, function() { return 1; });
-
-            for (var i = 0; i < totalBuildings; i++) {
-                _addBuilding(matrix);
-            }
-
-            return matrix;
-        }
-    };
-
-    var tilesByValue = {
+    var terrainByValue = {
         1: "grass",
         2: "path",
         3: "wall",
         4: "door"
     };
 
+    var initialTerrain = terrainByValue[1];
+
+    var VillageAlgorithm = {
+        generate: function(options) {
+            var diameter = options.diameter;
+            var totalBuildings = Math.floor(diameter / 10);
+            var buildingClearance = 2;
+            var matrix = new Matrix().init(diameter, diameter, function() { return initialTerrain; });
+
+            for (var i = 0; i < totalBuildings; i++) {
+                _addBuilding(matrix, buildingClearance);
+            }
+
+            return matrix;
+        }
+    };
+
     function _addBuilding(matrix, clearance) {
+        //Choose a random cell, check if the surrounding area is clear for a building,
+        //add building if clear, recurse if not
         var diameter = matrix.getWidth();
         var clearingDiameter = clearance * 2 + 1;
         var randomCell = matrix.getCell(Rand.rollFromZero(diameter), Rand.rollFromZero(diameter));
@@ -35,27 +38,25 @@ define(function(require) {
         var topLeftY = randomCell.y - clearance;
         var randomCellNeighbors = matrix.getSubMap(topLeftX, topLeftY, clearingDiameter, clearingDiameter);
 
-        if (randomCell === 1 && randomCellNeighbors.every(isClear)) {
+        if (randomCell === initialTerrain && randomCellNeighbors.every(isClear)) {
             var bTopLeftX = topLeftX + 1;
             var bTopLeftY = topLeftY + 1;
             var bDiameter = clearingDiameter - 2;
 
             for (var x = bTopLeftX; x < bTopLeftX + bDiameter; x++) {
                 for (var y = bTopLeftY; y < bTopLeftY + bDiameter; y++) {
-                    matrix.setCell(x, y, tilesByValue[3]);
+                    matrix.setCell(x, y, terrainByValue[3]);
                 }
             }
+        } else {
+            _addBuilding(matrix, clearance);
         }
 
         return matrix;
     }
 
     function isClear(cell) {
-        return cell === 1;
-    }
-
-    function smallNoise() {
-        return (Math.random() - 0.5) / 2;
+        return cell === initialTerrain;
     }
 
     return VillageAlgorithm;
